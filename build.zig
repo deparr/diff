d332b3ba4bbbacbbf9f0c9b284348a85a4294f1a
deparr <dparrott@protonmail.com>
2025-08-02 14:23:07 -0600

    build: add test step to build.zig

--- build.zig
+++ build.zig
@@ -4,14 +4,30 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const exe_mod = b.addModule("diff", .{
+    const lib_mod = b.addModule("zd", .{
+        .root_source_file = b.path("src/diff.zig"),
+    });
+
+    const lib_tests = b.addTest(.{
+        .root_source_file = b.path("src/diff.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run tests");
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+    test_step.dependOn(&run_lib_tests.step);
+
+    const exe_mod = b.addModule("zd-bin", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    exe_mod.addImport("zd", lib_mod);
+
     const exe = b.addExecutable(.{
-        .name = "diff",
+        .name = "zd",
         .root_module = exe_mod,
     });
 
